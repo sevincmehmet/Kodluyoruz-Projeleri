c8e7008a0bae4319cc527ea011cce536435cf291
Mehmet Ali SEVINC <kemalist611661@gmail.com>
2022-10-03 01:47:47 +0300

    odev 2 tamamlandı

--- JAVA/src/Main.java
+++ JAVA/src/Main.java
@@ -4,28 +4,29 @@ public class Main {
     public static void main(String[] args) {
         double fizik, kimya, mat;
 
-        Scanner klavye = new Scanner(System.in);
-
-        System.out.print("Lütfen matematik notunuzu giriniz :\t");
-        mat = klavye.nextDouble();
-        while(mat < 0  ||  mat > 100 ){
-            System.out.print("Geçersiz not girdiniz lütfen 0-100 arasnda gerçek notunuzu giriniz :\t");
+        try (Scanner klavye = new Scanner(System.in)) {
+            System.out.print("Lütfen matematik notunuzu giriniz :\t");
             mat = klavye.nextDouble();
-        }
+            while(mat < 0  ||  mat > 100 ){
+                System.out.print("Geçersiz not girdiniz lütfen 0-100 arasnda gerçek notunuzu giriniz :\t");
+                mat = klavye.nextDouble();
+            }
 
-        System.out.print("Lütfen fizik notunuzu giriniz :\t");
-        fizik = klavye.nextDouble();
-        while(fizik < 0  ||  fizik > 100 ){
-            System.out.print("Geçersiz not girdiniz lütfen 0-100 arasnda gerçek notunuzu giriniz :\t");
+            System.out.print("Lütfen fizik notunuzu giriniz :\t");
             fizik = klavye.nextDouble();
+            while(fizik < 0  ||  fizik > 100 ){
+                System.out.print("Geçersiz not girdiniz lütfen 0-100 arasnda gerçek notunuzu giriniz :\t");
+                fizik = klavye.nextDouble();
+            }
+
+            System.out.print("Lütfen kimya notunuzu giriniz :\t");
+            kimya = klavye.nextDouble();
+            while(kimya < 0  ||  kimya > 100 ){
+                System.out.print("Geçersiz not girdiniz lütfen 0-100 arasnda gerçek notunuzu giriniz :\t");
+                kimya = klavye.nextDouble();
+            }
         }
 
-        System.out.print("Lütfen kimya notunuzu giriniz :\t");
-        kimya = klavye.nextDouble();
-        while(kimya < 0  ||  kimya > 100 ){
-            System.out.print("Geçersiz not girdiniz lütfen 0-100 arasnda gerçek notunuzu giriniz :\t");
-            kimya = klavye.nextDouble();
-        }
         double ort = ((kimya + fizik + mat)/3);
         boolean kontrol = ort >= 60;
         String gectiKaldi = kontrol ? "Sınıfı Geçtiniz" : "Sınıfta Kaldınız";
