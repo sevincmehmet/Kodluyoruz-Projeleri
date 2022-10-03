6ae8bc9e73ab848ecdf79cf1f87d76c531edabbf
Mehmet Ali SEVINC <kemalist611661@gmail.com>
2022-10-03 14:55:19 +0300

    java ödev 4 tamamlandı

--- JAVA/src4/Main.java
+++ JAVA/src4/Main.java
@@ -1,10 +1,30 @@
 package JAVA.src4;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         
         //GİDİLEN MESAFEYE GÖRE TAKSİMETRE TUTTARI HESAPLAMA
 
+        Scanner klavye = new Scanner(System.in);
+        System.out.print("Lütfen kaç km gidildiğini giriniz\t:");
+
+        double yol = klavye.nextDouble();
+
+        while(yol<0){
+            System.err.print("Lütfen geçerli bir yol giriniz\t:");
+            yol = klavye.nextDouble();
+        }
+
+        double ücret = (10 +(yol*2.20));
         
+        if(ücret < 20){
+           ücret = 20;
+        }
+        
+        System.out.println("Ödenecek tutar\t:" + ücret);
+    
+
     }
 }
