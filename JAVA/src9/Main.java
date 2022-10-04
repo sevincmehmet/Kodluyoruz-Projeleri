78f520797fec42d5a92d05b9850a6cad73fbda73
Mehmet Ali <93881127+sevincmehmet@users.noreply.github.com>
2022-10-04 14:35:53 +0300

    Java ödev 9 tamamlandı

---
+++ JAVA/src9/Main.java
@@ -0,0 +1,36 @@
+package JAVA.src9;
+
+import java.util.Scanner;
+import java.util.Objects;
+
+public class Main {
+    public static void main(String[] args){
+
+        //KULLANNICI GİRİŞİ
+
+        Scanner sc = new Scanner(System.in);
+        String sifre = "Sevinc1967";
+        System.out.println("Şifrenizi giriniz : ");
+        String giris = sc.nextLine();
+
+        String secim[] = {"evet", "hayır"};
+        if(giris != sifre){
+            System.err.println("Şifreniz yanlış şifreyi sıfırlamak ister misiniz ? (evet veya hayır diye cevap veriniz..)");
+            String sec = sc.nextLine();
+            if(Objects.equals(sec, secim[0])) {
+                
+                System.out.println("yeni şifrenizi giriniz : ");
+                String yeniSifre = sc.nextLine();
+                
+                if(sifre.equals(yeniSifre)){
+                    
+                    while(sifre.equals(yeniSifre)){
+                        System.out.println("Yeni şifreniz eskisi ile aynı olamaz!!Şifre oluşturulamadı, lütfen başka şifre giriniz : ");
+                        yeniSifre = sc.nextLine();
+                    }System.out.println("Şifre oluşturuldu.");
+
+                }else System.out.println("Şifre oluşturuldu.");
+            }else System.out.println("peki :)");
+        }else System.out.println("Giriş yapıldı.");
+    }
+}
