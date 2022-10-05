6f043a89e5a51de9b802e455abde0c0c8850bc2a
Mehmet Ali <93881127+sevincmehmet@users.noreply.github.com>
2022-10-05 18:34:08 +0300

    Java ödev 14 tamamlandı

---
+++ JAVA/src14/CinZodyagi.java
@@ -0,0 +1,52 @@
+package JAVA.src14;
+import java.util.Scanner;
+
+public class CinZodyagi {
+    public static void main(String[] args) {
+        
+        //ÇİN ZODYAĞI BURCU HESPALAMA
+        try(Scanner scan = new Scanner(System.in)){
+            System.out.print("Doğum Yılınızı Giriniz : ");
+            int dogumYili = scan.nextInt();
+
+            switch(dogumYili % 12){
+                case 0 :
+                    System.out.println("Çin zodyağı burcunuz : Maymun"); 
+                break;
+                case 1 :
+                    System.out.println("Çin zodyağı burcunuz : Horoz"); 
+                break;
+                case 2 :
+                    System.out.println("Çin zodyağı burcunuz : Köpek"); 
+                break;
+                case 3 :
+                    System.out.println("Çin zodyağı burcunuz : Domuz"); 
+                break;
+                case 4 :
+                    System.out.println("Çin zodyağı burcunuz : Fare"); 
+                break;
+                case 5 :
+                    System.out.println("Çin zodyağı burcunuz : Öküz"); 
+                break;
+                case 6 :
+                    System.out.println("Çin zodyağı burcunuz : Kaplan"); 
+                break;
+                case 7 :
+                    System.out.println("Çin zodyağı burcunuz : Tavşan"); 
+                break;
+                case 8 :
+                    System.out.println("Çin zodyağı burcunuz : Ejderha"); 
+                break;
+                case 9 :
+                    System.out.println("Çin zodyağı burcunuz : Yılan"); 
+                break;
+                case 10 :
+                    System.out.println("Çin zodyağı burcunuz : At"); 
+                break;
+                case 11:
+                    System.out.println("Çin zodyağı burcunuz : Koyun"); 
+                break;
+            }
+        }
+    }
+}
