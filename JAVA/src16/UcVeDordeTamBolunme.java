3a929f4844cfb08bb679f63f3c50093d37bf136d
Mehmet Ali <93881127+sevincmehmet@users.noreply.github.com>
2022-10-06 15:36:50 +0300

    Java ödev 17 tamamlandı

--- JAVA/src16/UcVeDordeTamBolunme.java
+++ JAVA/src16/UcVeDordeTamBolunme.java
@@ -7,12 +7,12 @@ public class UcVeDordeTamBolunme {
         //GİRİLEN SAYIYA KADAR 3 VE 4 E TAM BÖLÜNENLERİN ORTALAMASI
         int n;
         double toplam = 0;
-        Scanner scan = new Scanner(System.in);
-
-        do{
-            System.out.print("Pozitif sayı giriniz : ");
-            n = scan.nextInt();
-        }while(n < 0);
+        try (Scanner scan = new Scanner(System.in)) {
+            do{
+                System.out.print("Pozitif sayı giriniz : ");
+                n = scan.nextInt();
+            }while(n < 0);
+        }
 
         for (int i = 0; i < n; i++) {
             if(i%3 == 0 && i%4 == 0 ){
