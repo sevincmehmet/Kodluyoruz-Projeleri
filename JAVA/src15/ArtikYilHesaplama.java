b7dccc84f5246818e4573090ece2e7c35502ac7a
Mehmet Ali <93881127+sevincmehmet@users.noreply.github.com>
2022-10-06 14:41:27 +0300

    Java ödev 15 tamamlandı

---
+++ JAVA/src15/ArtikYilHesaplama.java
@@ -0,0 +1,27 @@
+package JAVA.src15;
+import java.util.Scanner;
+
+public class ArtikYilHesaplama {
+    public static void main(String[] args) {
+
+        try (Scanner scan = new Scanner(System.in)) {
+            int yil;
+            String stringYil;
+            System.out.print("Yıl Giriniz : ");
+            yil = scan.nextInt();
+            
+            if(yil < 0){
+               stringYil = "Milattan önce ";
+               yil *= -1;
+            }else stringYil = "";
+
+            if(yil % 4 == 0){
+                System.out.println(stringYil + yil + " bir artık yıldır ! ");
+            }else System.out.println(stringYil + yil + " bir artık yıl değildir ! ");
+        }
+
+        
+    
+    }
+    
+}
