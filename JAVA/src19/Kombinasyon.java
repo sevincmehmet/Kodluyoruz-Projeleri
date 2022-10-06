7c9710a2d656e7767864c1ad224fd58f544da6ad
Mehmet Ali <93881127+sevincmehmet@users.noreply.github.com>
2022-10-06 18:49:26 +0300

    Java ödev 20 tamamlandı

--- JAVA/src19/Kombinasyon.java
+++ JAVA/src19/Kombinasyon.java
@@ -5,19 +5,19 @@ import java.util.Scanner;
 public class Kombinasyon {
     public static void main(String[] args) {
         int n, r;
-        Scanner scan = new Scanner(System.in);
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println("Kombinasyonunu öğrenmek isteddiğniz sayıları C(n,r) şeklinde giriniz.");
+            do{
+                System.out.print("Lütfen negatif giriş yapmayınız n ? : ");
+                n = scan.nextInt();
+            }while(n<0);
+            
+            do{
+                System.out.print("Lütfen negatif giriş yapmayınız r ? : ");
+                r = scan.nextInt();
+            }while(n<0);
+        }
         
-        System.out.println("Kombinasyonunu öğrenmek isteddiğniz sayıları C(n,r) şeklinde giriniz.");
-        do{
-            System.out.print("Lütfen negatif giriş yapmayınız n ? : ");
-            n = scan.nextInt();
-        }while(n<0);
-        
-        do{
-            System.out.print("Lütfen negatif giriş yapmayınız r ? : ");
-            r = scan.nextInt();
-        }while(n<0);
-
         System.out.println("Cevap : " + combination(n, r));
     }
 
