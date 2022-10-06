b42faa27a69d57bcec9117f1ae74e4f1a38ccb57
Mehmet Ali <93881127+sevincmehmet@users.noreply.github.com>
2022-10-06 16:03:05 +0300

    açıklama satırı eklendi

--- JAVA/src18/Main.java
+++ JAVA/src18/Main.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        //4 VE 5' İN KUVVETLERİNİ EKRANA YADIRAN PROGRAM
         int sinir;
         try (Scanner scan = new Scanner(System.in)) {
             sinir = scan.nextInt();
@@ -14,7 +16,7 @@ public class Main {
         for (int i = 1; i < sinir; i*=4) {
             System.out.print(i + " ");
         }
-        
+
         System.out.println();
 
         System.out.print(sinir + " sayısına kadar olan 5 ün kuvvetleri : " );
