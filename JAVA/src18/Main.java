9273a4b7498ac62e7121eb94fba0000d79a2c02e
Mehmet Ali <93881127+sevincmehmet@users.noreply.github.com>
2022-10-06 16:00:39 +0300

    Java ödev 18 tamamlandı

---
+++ JAVA/src18/Main.java
@@ -0,0 +1,28 @@
+
+package JAVA.src18;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int sinir;
+        try (Scanner scan = new Scanner(System.in)) {
+            sinir = scan.nextInt();
+        }
+        System.out.print(sinir + " sayısına kadar olan 4 ün kuvvetleri : " );
+
+        for (int i = 1; i < sinir; i*=4) {
+            System.out.print(i + " ");
+        }
+        
+        System.out.println();
+
+        System.out.print(sinir + " sayısına kadar olan 5 ün kuvvetleri : " );
+
+        for (int i = 1; i < sinir; i*=5) {
+            System.out.print(i + " ");
+        }
+
+
+    }
+}
