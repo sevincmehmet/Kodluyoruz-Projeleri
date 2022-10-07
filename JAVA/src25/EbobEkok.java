b80d34656aa0fcbc5df8ed1b5f624ae909926312
Mehmet Ali <93881127+sevincmehmet@users.noreply.github.com>
2022-10-08 01:58:09 +0300

    girilen sayılardan en büyük ve en kücüğü bulma

--- JAVA/src25/EbobEkok.java
+++ JAVA/src25/EbobEkok.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 public class EbobEkok {
     public static void main(String[] args) {
         int s1, s2; 
-        Scanner inp = new Scanner(System.in);
-        System.out.println("Lütfen ebobunu ve ekokunu bulmak istediğiniz sayıların  değerlerini giriniz : ");
-        s1 = inp.nextInt();
-        s2 = inp.nextInt();
-
+        try (Scanner inp = new Scanner(System.in)) {
+            System.out.println("Lütfen ebobunu ve ekokunu bulmak istediğiniz sayıların  değerlerini giriniz : ");
+            s1 = inp.nextInt();
+            s2 = inp.nextInt();
+        }
         System.out.println("EBOB : " + ebob(s1, s2) +
                     "\nEKOK : " + ekok(s1, s2));
     }
