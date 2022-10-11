9bec696119045975297adb47f073331f1c5b9329
Mehmet Ali <93881127+sevincmehmet@users.noreply.github.com>
2022-10-11 14:40:09 +0300

    random dizideki tekrar eden tek ve ççift ssayılar

---
+++ JAVA/tekrar eden çift sayi/Tekrar.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+import javax.swing.text.html.FormView;
+
+public class Tekrar {
+    public static void main (String[] args){
+
+        //Bir sayı dizisindeki tekrar eden çift sayıları belirten program
+
+        int[] arr = new int[5];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int)(Math.random()*10+1);
+        }
+
+        System.out.print("Diziniz : " + Arrays.toString(arr));
+        System.out.print("\nTekrar eden çift sayılar : ");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if((arr[i] % 2 == 0) && (arr[i] == arr[j])){
+                    System.out.print(arr[i] + " ");
+                }
+            }
+        }
+        System.out.print("\nTekrar eden tek sayılar : ");
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                if((arr[i] % 2 == 1) && (arr[i] == arr[j])){
+                    System.out.print(arr[i] + " ");
+                }
+            }
+        }
+    }
+}
