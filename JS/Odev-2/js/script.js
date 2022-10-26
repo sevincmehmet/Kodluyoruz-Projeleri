d261089ad1cadc68fa112ce67058206388d1ec45
Mehmet Ali <kemalist611661@gmail.com>
2022-10-27 02:47:25 +0300

    Local storage : yenilenince veriler ekranda kaldı

--- JS/Odev-2/js/script.js
+++ JS/Odev-2/js/script.js
@@ -1,10 +1,20 @@
+let arrayList = localStorage.getItem("IsArrayList") ? JSON.parse(localStorage.getItem("IsArrayList")) : [] ;
+
+//DOM ile html etiiketlerine ulaştık
 let btnDOM = document.querySelector('#liveToastButton')
 let taskDOM = document.querySelector('#task')
 let listDOM = document.querySelector('#list')
 
+let liDOM = document.createElement('li'); // Yeni liste elemanları oluşturduk
 
-localStorage.setItem('items', JSON.stringify(listArray));
-const data = JSON.parse(localStorage.getItem('items'));
+let data = JSON.parse(localStorage.getItem('IsArrayList'));
+
+data.forEach(IsArrayList => {
+    liDOM = document.createElement('li')
+    liDOM.textContent = IsArrayList;
+    listDOM.appendChild(liDOM)
+});
+
 
 function check() {
     this.classList.toggle("checked"); // toggle aktif pasif durumunu belirtir aktifken checked clasını ekle :)
@@ -12,18 +22,23 @@ function check() {
 function removeButton() {
     this.parentElement.remove(); //çarpının bulundugu maddeyi silmek için kullanılır    
 }
+let sayac = 0;
 
 function newElement() {
     if (!(Boolean(task.value.trim())))  {  // input değeri boş girildiğinde ve girilmediğinde 
         $(".error").toast("show"); //error clasını kullanarak 
       } else {
         $(".success").toast("show");
-
-        let liDOM = document.createElement('li'); // Yeni liste elemanları oluşturduk
-        listDOM.appendChild(liDOM); // Her eklenen liste elemanlarını list imizin en sonuna koydul
-        liDOM.textContent = task.value; // Eklenen eleman içine girilen değeri yadırdık
-        taskDOM.value = ""; // Eklendikten sonra task ımınızın içindeki bilgiyi sildirdik
         
+        arrayList.push(taskDOM.value); //dizinin içine girilen elemanı ekledik
+        localStorage.setItem('IsArrayList', JSON.stringify(arrayList)) // diziyi string ifadeye dönüştürdük ve kaydettik
+        
+
+        liDOM = document.createElement('li')
+        listDOM.appendChild(liDOM); // Her eklenen liste elemanlarını list imizin en sonuna koydul
+        liDOM.textContent = task.value; // Eklenen eleman içine girilen değeri yazdırdık
+
+        taskDOM.value = ""; // Eklendikten sonra task ımınızın içindeki bilgiyi sildirdik
         liDOM.onclick = check;
 
         let closeBtn = document.createElement('span');// span olusturduk
